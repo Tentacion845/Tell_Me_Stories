b6446ae6d837e3103b1500b6deb15ce3abf19525
Hadel <ladghem.hadel@gmail.com>
2023-05-04 09:24:25 +0200

    ajout promotion role en bdd

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
